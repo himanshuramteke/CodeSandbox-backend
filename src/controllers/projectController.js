4a88cc7bbf91c601f4c99d6f73d77c7b6a8bb1fc
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-04-24 12:33:45 +0530

    Added get project tree

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,23 +1,20 @@
-import uuid4 from "uuid4";
-import util from 'util';
-import child_process from "child_process";
-import fs from 'fs/promises';
+import { createProjectService, getProjectTreeService } from "../service/projectService.js";
 
-const execPromisified = util.promisify(child_process.exec);
 
 export const createProjectController = async (req, res) => {
     
-    //create a unique id and then inside the projects folder create a new folder with that id
-    const projectId = uuid4();
-    console.log('New Project id is', projectId);
-
-    await fs.mkdir(`./projects/${projectId}`);
-
-    //After this call the npm create vite command in the newly created project folder
-    const response = await execPromisified('npm create vite@latest sandbox -- --template react', {
-        cwd: `./projects/${projectId}`
-    })
-
+    const projectId = await createProjectService();
+   
     return res.json({ message: 'Project created', data: projectId});
     
+}
+
+export const getProjectTreeController = async (req, res) => {
+
+    const tree = await getProjectTreeService(req.params.projectId);
+    return res.json(200).json({
+        data: tree,
+        success: true,
+        message: 'Successfully fetched the tree'
+    })
 }
